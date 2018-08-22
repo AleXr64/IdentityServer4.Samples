d79c3dbfd72423c71c4fe9dfb4e8b0c4bc66e205
Amel Salibasic <nukecx@gmail.com>
2018-08-22 20:16:51 +0100

    updated correct addauthentication middleware settings for identity

--- Quickstarts/6_AspNetIdentity/src/IdentityServerWithAspNetIdentity/Startup.cs
+++ Quickstarts/6_AspNetIdentity/src/IdentityServerWithAspNetIdentity/Startup.cs
@@ -35,6 +35,23 @@ namespace IdentityServerWithAspNetIdentity
                             .AddSignInManager<SignInManager<ApplicationUser>>()
                             .AddDefaultTokenProviders();
 
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            }).AddCookie(IdentityConstants.ApplicationScheme, options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromHours(1);
+                options.LoginPath = "/Account/Signin";
+                options.LogoutPath = "/Account/Signout";
+            }).AddCookie(IdentityConstants.ExternalScheme, o =>
+            {
+                o.Cookie.Name = IdentityConstants.ExternalScheme;
+                o.ExpireTimeSpan = TimeSpan.FromMinutes(5.0);
+            });
+
             services.AddMvc();
 
             services.Configure<IISOptions>(iis =>
